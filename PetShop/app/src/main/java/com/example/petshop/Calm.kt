0cf4d3bb3b4572070ceb48e4d90b3eceafe9ab9d
mjaxin <mpjackso@ualberta.ca>
2026-09-04 14:07:02 -0600

    did participation exercide

---
+++ PetShop/app/src/main/java/com/example/petshop/Calm.kt
@@ -0,0 +1,7 @@
+package com.example.petshop
+
+class Calm(date: String): Mood(date){
+    override fun represent(): String {
+        return "yo I'm so at peace rn"
+    }
+}
---
+++ PetShop/app/src/main/java/com/example/petshop/Melancholy.kt
@@ -0,0 +1,7 @@
+package com.example.petshop
+
+class Melancholy(date: String) : Mood(date)  {
+    override fun represent(): String {
+        return "yo I'm a lil sad rn"
+    }
+}
